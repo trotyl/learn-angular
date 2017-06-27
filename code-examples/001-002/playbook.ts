7632dcf2499d136bd2461c5356ee51baf81af531
Trotyl <trotyl@qq.com>
2017-06-27 14:25:32 +0800

    refactor playbook for 001-002

--- code-examples/001-002/playbook.ts
+++ code-examples/001-002/playbook.ts
@@ -6,24 +6,36 @@ function stage(name: string, task: () => void): void {
   task()
 }
 
-function checkFileExists(path): void {
+function checkFileExists(path: string): void {
   if (!fs.existsSync(path)) {
     throw new Error(`${path} not found!`)
   }
 }
 
-stage('Starting check for 001-002', () => {
-  shell.cd('code-examples/001-002')
-})
+function playbook(name: string, path: string, task: () => void): void {
+  const pwd = shell.pwd()
 
-stage('Creating new directory', () => {
-  shell.rm('-rf', 'learn-angular-ci')
-  shell.mkdir('learn-angular-ci')
-  shell.cd('learn-angular-ci')
-})
+  shell.echo(`Starting playbook for ${name}`)
+  shell.cd(path)
 
-stage('Adding app files with static properties', () => {
-  const appComponentJs = `\
+  task()
+  shell.echo('Playbook passed')
+
+  shell.cd(pwd)
+}
+
+function initWorkspace(name: string): void {
+  shell.echo(`Creating workspace ${name}`)
+  shell.rm('-rf', name)
+  shell.mkdir(name)
+  shell.cd(name)
+}
+
+playbook('001-002', 'code-examples/001-002', () => {
+  initWorkspace('learn-angular-ci')
+
+  stage('Adding app files with static properties', () => {
+    const appComponentJs = `\
 import { Component } from '@angular/core'
 
 export class AppComponent { }
@@ -35,7 +47,7 @@ AppComponent.annotations = [
   })
 ]
 `
-  const appModuleJs = `\
+    const appModuleJs = `\
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { AppComponent } from './app.component'
@@ -56,42 +68,42 @@ AppModule.annotations = [
   })
 ]
 `
-  const mainJs = `\
+    const mainJs = `\
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
 import { AppModule } from './app.module'
 
 platformBrowserDynamic().bootstrapModule(AppModule)
 `
 
-  fs.writeFileSync('app.component.js', appComponentJs)
-  fs.writeFileSync('app.module.js', appModuleJs)
-  fs.writeFileSync('main.js', mainJs)
-})
+    fs.writeFileSync('app.component.js', appComponentJs)
+    fs.writeFileSync('app.module.js', appModuleJs)
+    fs.writeFileSync('main.js', mainJs)
+  })
 
-stage('Installing dependencies by Yarn', () => {
-  shell.exec('yarn add @angular/core @angular/common @angular/compiler @angular/platform-browser @angular/platform-browser-dynamic rxjs')
-})
+  stage('Installing dependencies by Yarn', () => {
+    shell.exec('yarn add @angular/core @angular/common @angular/compiler @angular/platform-browser @angular/platform-browser-dynamic rxjs')
+  })
 
-stage('Bundling app using Webpack', () => {
-  shell.exec('webpack main.js bundle-0.js')
-})
+  stage('Bundling app using Webpack', () => {
+    shell.exec('webpack main.js bundle-0.js')
+  })
 
-stage('Checking webpack results', () => {
-  checkFileExists('./bundle-0.js')
-  shell.echo('Webpack bundle generated')
-})
+  stage('Checking webpack results', () => {
+    checkFileExists('./bundle-0.js')
+    shell.echo('Webpack bundle generated')
+  })
 
-stage('Transpile JavaScripts using tsc for static properties', () => {
-  shell.exec('tsc --allowJs --outDir dist-0 --target es2015 main.js')
-})
+  stage('Transpile JavaScripts using tsc for static properties', () => {
+    shell.exec('tsc --allowJs --outDir dist-0 --target es2015 main.js')
+  })
 
-stage('Checking tsc results for static properties', () => {
-  checkFileExists('./dist-0/app.component.js')
-  shell.echo('TypeScript Compilation works')
-})
+  stage('Checking tsc results for static properties', () => {
+    checkFileExists('./dist-0/app.component.js')
+    shell.echo('TypeScript Compilation works')
+  })
 
-stage('Modifying source files with Decorator', () => {
-  const appComponentJs = `\
+  stage('Modifying source files with Decorator', () => {
+    const appComponentJs = `\
 import { Component } from '@angular/core'
 
 @Component({
@@ -100,7 +112,7 @@ import { Component } from '@angular/core'
 })
 export class AppComponent { }
 `
-  const appModuleJs = `\
+    const appModuleJs = `\
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { AppComponent } from './app.component'
@@ -118,40 +130,39 @@ import { AppComponent } from './app.component'
 })
 export class AppModule { }
 `
-  fs.writeFileSync('app.component.js', appComponentJs)
-  fs.writeFileSync('app.module.js', appModuleJs)
-})
+    fs.writeFileSync('app.component.js', appComponentJs)
+    fs.writeFileSync('app.module.js', appModuleJs)
+  })
 
-stage('Transpile JavaScripts using tsc for Decorators', () => {
-  shell.exec('tsc --allowJs --outDir dist-1 --target es2015 --experimentalDecorators main.js')
-})
+  stage('Transpile JavaScripts using tsc for Decorators', () => {
+    shell.exec('tsc --allowJs --outDir dist-1 --target es2015 --experimentalDecorators main.js')
+  })
 
-stage('Bundling app using Webpack', () => {
-  shell.exec('webpack dist-1/main.js bundle-1.js')
-})
+  stage('Bundling app using Webpack', () => {
+    shell.exec('webpack dist-1/main.js bundle-1.js')
+  })
 
-stage('Checking webpack results', () => {
-  checkFileExists('./bundle-1.js')
-  shell.echo('Webpack bundle generated')
-})
+  stage('Checking webpack results', () => {
+    checkFileExists('./bundle-1.js')
+    shell.echo('Webpack bundle generated')
+  })
 
-stage('Upgrading source files to TypeScript', () => {
-  shell.mv('app.component.js', 'app.component.ts')
-  shell.mv('app.module.js', 'app.module.ts')
-  shell.mv('main.js', 'main.ts')
-})
+  stage('Upgrading source files to TypeScript', () => {
+    shell.mv('app.component.js', 'app.component.ts')
+    shell.mv('app.module.js', 'app.module.ts')
+    shell.mv('main.js', 'main.ts')
+  })
 
-stage('Transpile TypeScripts using tsc', () => {
-  shell.exec('tsc --target es2015 --experimentalDecorators --moduleResolution node main.ts')
-})
+  stage('Transpile TypeScripts using tsc', () => {
+    shell.exec('tsc --target es2015 --experimentalDecorators --moduleResolution node main.ts')
+  })
 
-stage('Bundling app using Webpack', () => {
-  shell.exec('webpack main.js bundle-2.js')
-})
+  stage('Bundling app using Webpack', () => {
+    shell.exec('webpack main.js bundle-2.js')
+  })
 
-stage('Checking webpack results', () => {
-  checkFileExists('./bundle-2.js')
-  shell.echo('Webpack bundle generated')
+  stage('Checking webpack results', () => {
+    checkFileExists('./bundle-2.js')
+    shell.echo('Webpack bundle generated')
+  })
 })
-
-shell.echo('All process checked')
